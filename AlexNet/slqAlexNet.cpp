be2cdb544ae87be1938243c3bf3e385fdbdae9f3
aitazhixin <aitazhixin@users.noreply.github.com>
2017-06-08 20:03:42 +0800

    Create slqAlexNet.cpp

---
+++ AlexNet/slqAlexNet.cpp
@@ -0,0 +1,113 @@
+
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "slqAlexNet.h"
+
+using namespace std;
+
+namespace slqDL {
+    namespace slqAlexNet {
+        slqAlexNet::~slqAlexNet()
+        {
+            deletevar(inMap);
+            deletevar(c1Map;
+            deletevar(s1Map;
+            deletevar(c2Map;
+            deletevar(s2Map;
+            deletevar(c3Map;
+            deletevar(c4Map;
+            deletevar(c5Map;
+            deletevar(s5Map;
+            deletevar(f1Map;
+            deletevar(f2Map;
+            deletevar(f3Map;
+            
+            deletevar(c1Conv;
+            deletevar(s1Pool;
+            deletevar(c2Conv;
+            deletevar(s2Pool;
+            float *c3Conv;
+            float *c4Conv;
+            float *c5Conv;
+            float *s5Pool;
+            float *f1Conn;
+            float *f2Conn;
+            float *f3Conn;
+            
+            float *c1Bias;
+            float *s1Bias;
+            float *c2Bias;
+            float *s2Bias;
+            float *c3Bias;
+            flaot *c4Bias;
+            float *c5Bias;
+            float *s5Bias;
+            float *f1Bias;
+            float *f2Bias;
+            float *f3Bias;
+            
+            float *c1MapDt;
+            float *s1MapDt;
+            float *c2MapDt;
+            float *s2MapDt;
+            float *c3MapDt;
+            float *c4MapDt;
+            float *c5MapDt;
+            float *s5MapDt;
+            float *f1MapDt;
+            float *f2MapDt;
+            float *f3MapDt;
+            
+            float *c1ConvDt;
+            float *s1PoolDt;
+            float *c2ConvDt;
+            float *s2PoolDt;
+            float *c3ConvDt;
+            float *c4ConvDt;
+            float *c5ConvDt;
+            float *s5PoolDt;
+            float *f1ConnDt;
+            float *f2ConnDt;
+            float *f3ConnDt;
+            
+            float *c1BiasDt;
+            float *s1BiasDt;
+            float *c2BiasDt;
+            float *s2BiasDt;
+            deletevar(c3BiasDt;
+            deletevar(c4BiasDt;
+            deletevar(c5BiasDt;
+            deletevar(s5BiasDt;
+            deletevar(f1BiasDt;
+            deletevar(f2BiasDt;
+            deletevar(f3BiasDt;
+            
+            deletevar(c1ConvEDt;
+            deletevar(s1PoolEDt;
+            deletevar(c2ConvEDt;
+            deletevar(s2PoolEDt;
+            deletevar(c3ConvEDt;
+            deletevar(c4ConvEDt;
+            deletevar(c5ConvEDt;
+            deletevar(s5PoolEDt;
+            deletevar(f1ConnEDt;
+            deletevar(f2ConnEDt;
+            deletevar(f3ConnEDt;
+            
+            deletevar(c1BiasEDt;
+            deletevar(s1BiasEDt;
+            deletevar(c2BiasEDt;
+            deletevar(s2BiasEDt;
+            deletevar(c3BiasEDt;
+            deletevar(c4BiasEDt;
+            deletevar(c5BiasEDt;
+            deletevar(s5BiasEDt;
+            deletevar(f1BiasEDt;
+            deletevar(f2BiasEDt;
+            deletevar(f3BiasEDt;
+        }
+    }
+}
